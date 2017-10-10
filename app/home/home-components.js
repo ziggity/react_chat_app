b6753e0d5409d62f148206b0edbb0124498db756
zach beecher <zachbeecher@gmail.com>
2017-10-09 23:26:10 -0700

    works with node server.js and navigate to localhost:3000

---
+++ app/home/home-components.js
@@ -0,0 +1,98 @@
+//Create Login
+//Create Message Component
+//Style Components
+
+var Chat = React.createClass({
+  getInitialState: function(){
+    return {
+      socket: io(),
+      username: localStorage.getItem('username') || "guest",
+      messages: [],
+      users: []
+    }
+  },
+  componentDidMount: function(){
+    var self = this;
+    this.state.socket.on('receive-message', function(message){
+      var messages = self.state.messages;
+          messages.push(message);
+      self.setState({messages: messages});
+    });
+
+    this.state.socket.on('users-updated', function(users){
+      self.setState({users: users});
+    });
+
+    if (this.state.username !== "guest"){
+      var username = JSON.parse(this.state.username);
+      this.setState({username: username});
+      this.state.socket.emit('join', username);
+    }
+  },
+  login: function(){
+    var self = this;
+    var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "/signup");
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.onreadystatechange = function(){
+          if (xhttp.readyState === 4 && xhttp.status === 200){
+            console.log(xhttp.responseText);
+            self.state.socket.emit('join', document.getElementById('username').value);
+            self.setState({username: document.getElementById('username').value});
+            localStorage.setItem('username', JSON.stringify(document.getElementById('username').value));
+
+          }
+        };
+        xhttp.send(JSON.stringify({
+          username: document.getElementById('username').value,
+          password: document.getElementById('password').value
+        }));
+  },
+  sendMessage: function(){
+    var self = this;
+    var message = {
+       username: self.state.username,
+       message: document.getElementById('message').value
+     };
+    self.state.socket.emit('message', message);
+    document.getElementById('message').value = "";
+  },
+  enterMessage: function(event){
+    var self = this;
+    if (event.which === 13 && document.getElementById('message').value.length > 0){
+      var message = {
+        username: self.state.username,
+        message: document.getElementById('message').value
+      };
+      self.state.socket.emit('message', message);
+      document.getElementById('message').value = "";
+    }
+  },
+  render: function(){
+    var messages = this.state.messages.map(function(message){
+      return (<li className="message"><strong>{message.username}: </strong> <span>{message.message}</span></li>)
+    });
+    var users = this.state.users.map(function(user){
+      return(<li>{user}</li>)
+    });
+    var userLogin;
+    var userCount = this.state.users.length;
+    if (this.state.username === "guest"){
+      userLogin =<div> <input type="text" placeholder="username" id="username"/> <input placeholder="password" type="password" id="password"/> <button onClick={this.login}> Login </button></div>;
+    } else {
+      userLogin = <div> <strong> You are logged in as {this.state.username} </strong> </div>
+    }
+    return(<div>
+      {userLogin} <br/>
+      <ul className="chat">{messages}</ul>
+       <ul className="user-list">
+         <li>users({userCount})</li>
+         {users}
+       </ul>
+     <br/>
+      <input type="text" id="message" onKeyDown={this.enterMessage}/> <button id="send" onClick={this.sendMessage}> Send </button>
+    </div>)
+  }
+});
+
+ReactDOM.render(<Chat/>, document.getElementById('chat'));
--- server.js
+++ server.js
@@ -1,4 +1,63 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io');
+var express = require('express'),
+    app     = express(),
+    http    = require('http'),
+    socketIO      = require('socket.io'),
+    mongoose      = require('mongoose'),
+    bodyParser    = require('body-parser');
+    //http.listen(process.env.PORT || 5000);
+
+const PORT = process.env.PORT || 3000;
+const INDEX = __dirname + '/index.html';
+var server = http.createServer(app);
+const io = socketIO.listen(server);
+server.listen(PORT);
+mongoose.connect(process.env.MONGOLINK);
+var people = {};
+var clientUsers = [];
+
+
+
+var userController = require('./server/controllers/user-controller.js');
+
+io.on('connection', (socket)=> {
+  console.log("Client Connected");
+  socket.on('join', (username) =>{
+    updatePeople(username, socket.id);
+    io.emit('users-updated', clientUsers);
+  });
+  socket.on('disconnect', () => {
+    console.log("Client Disconnected");
+    delete people[socket.id];
+  });
+  socket.on('message', (message)=> io.emit('receive-message', message));
+});
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+    });
+
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use('/app', express.static(__dirname + '/app'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+//User Create
+app.post('/signup', userController.signup);
+
+
+//functions
+function updatePeople(username, id){
+  var userArray = [];
+  for (key in people){
+    if (people[key] === username){
+      return;
+    }
+    userArray.push(people[key]);
+  }
+  userArray.push(username);
+  people[id] = username;
+  clientUsers = userArray;
+}
